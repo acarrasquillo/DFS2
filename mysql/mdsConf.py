5899dc2cf7e504b664112186cb0cf5123f7a3081
acarrasquillo <abimael.carrasquillo@gmail.com>
2013-12-06 00:33:08 -0400

    validateIP

--- mysql/mdsConf.py
+++ mysql/mdsConf.py
@@ -1,15 +1,17 @@
 import os
-from socket import inet_aton, error
 mds_PORT = 1000
 mds_HOST = "localhost"
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-def ValidateIP(address):
-	if 'localhost' == address:
-		return True 
-
-	try:
-		inet_aton(address)
+def validateIP(address):
+	if address=='localhost':
 		return True
-	except error:
-		return False
+	else:
+	    try:
+	        host_bytes = address.split('.')
+	        valid = [int(b) for b in host_bytes]
+	        valid = [b for b in valid if b >= 0 and b<=255]
+	        return len(host_bytes) == 4 and len(valid) == 4
+
+	    except:
+	        return False
